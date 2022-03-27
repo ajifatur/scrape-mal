4aa92f623c883b53d2589f84e10e3f39b9584ae2
Aji Fatur <ajifatur32@gmail.com>
2022-03-27 12:32:21 +0700

    Add people, characters, companies

--- controllers/index.js
+++ controllers/index.js
@@ -22,7 +22,10 @@ export const Endpoints = (req, res) => {
         {path: '/top/manga/manhwa', title: 'Top Manhwa'},
         {path: '/top/manga/manhua', title: 'Top Manhua'},
         {path: '/top/manga/bypopularity', title: 'Most Popular Manga'},
-        {path: '/top/manga/favorite', title: 'Most Favorited Manga'}
+        {path: '/top/manga/favorite', title: 'Most Favorited Manga'},
+        {path: '/people', title: 'People'},
+        {path: '/characters', title: 'Characters'},
+        {path: '/companies', title: 'Companies'}
     ];
     res.send(paths);
 }
@@ -41,10 +44,10 @@ export const Top = (req, res) => {
                     lists[i] = {
                         rank: $(elem).find('.rank .top-anime-rank-text').text(),
                         title: $(elem).find('.title .detail h3 a').text(),
-                        image: $(elem).find('.title img').attr('data-src'),
+                        information: $(elem).find('.title .detail .information').text().replace(/  +/g, '').split('\n').filter(n => n != ''),
+                        score: $(elem).find('.score .score-label').text(),
                         permalink: $(elem).find('.title .detail h3 a').attr('href'),
-                        information: $(elem).find('.title .detail .information').text().trim(),
-                        score: $(elem).find('.score .score-label').text()
+                        image: $(elem).find('.title img').attr('data-src')
                     }
                 });
                 lists = lists.filter(n => n !== undefined);
@@ -54,4 +57,126 @@ export const Top = (req, res) => {
         .catch(error => {
             console.log(error);
         });
+}
+
+export const People = (req, res) => {
+    let url = 'https://myanimelist.net/people.php';
+
+    axios
+        .get(url)
+        .then(response => {
+            if(response.status === 200) {
+                const $ = cheerio.load(response.data);
+                let lists = [];
+                $('.people-favorites-ranking-table .ranking-list').each(function(i, elem) {
+                    lists[i] = {
+                        rank: $(elem).find('.rank span').text(),
+                        name: $(elem).find('.people .information a').text(),
+                        alias: $(elem).find('.people .information span').text(),
+                        birthday: $(elem).find('.birthday').text().replace(/\s\s+/g, ' ').trim(),
+                        favorites: $(elem).find('.favorites').text().replace(/\s\s+/g, ' ').trim(),
+                        permalink: $(elem).find('.people .information a').attr('href'),
+                        image: $(elem).find('.people img').attr('data-src')
+                    }
+                });
+                lists = lists.filter(n => n !== undefined);
+                res.send(lists);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+export const Characters = (req, res) => {
+    let url = 'https://myanimelist.net/character.php';
+
+    axios
+        .get(url)
+        .then(response => {
+            if(response.status === 200) {
+                const $ = cheerio.load(response.data);
+                let lists = [];
+                $('.characters-favorites-ranking-table .ranking-list').each(function(i, elem) {
+                    let animeography = [];
+                    $(elem).find('.animeography .title').each(function(j, elem2) {
+                        animeography[j] = {
+                            name: $(elem2).find('a').text(),
+                            permalink: $(elem2).find('a').attr('href')
+                        }
+                    });
+
+                    let mangaography = [];
+                    $(elem).find('.mangaography .title').each(function(j, elem2) {
+                        mangaography[j] = {
+                            name: $(elem2).find('a').text(),
+                            permalink: $(elem2).find('a').attr('href')
+                        }
+                    });
+
+                    lists[i] = {
+                        rank: $(elem).find('.rank span').text(),
+                        name: $(elem).find('.people .information a').text(),
+                        alias: $(elem).find('.people .information span').text(),
+                        animeography: animeography,
+                        mangaography: mangaography,
+                        favorites: $(elem).find('.favorites').text().replace(/\s\s+/g, ' ').trim(),
+                        permalink: $(elem).find('.people .information a').attr('href'),
+                        image: $(elem).find('.people img').attr('data-src')
+                    }
+                });
+                lists = lists.filter(n => n !== undefined);
+                res.send(lists);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
+}
+
+export const Companies = (req, res) => {
+    let url = 'https://myanimelist.net/company';
+
+    axios
+        .get(url)
+        .then(response => {
+            if(response.status === 200) {
+                const $ = cheerio.load(response.data);
+                let lists = [];
+                $('.company-favorites-ranking-table .ranking-list').each(function(i, elem) {
+                    let mostPopulars = [];
+                    $(elem).find('.popularity p').each(function(j, elem2) {
+                        mostPopulars[j] = {
+                            name: $(elem2).find('a').text(),
+                            permalink: $(elem2).find('a').attr('href')
+                        }
+                    });
+
+                    let topScorers = [];
+                    $(elem).find('.score p').each(function(j, elem2) {
+                        topScorers[j] = {
+                            name: $(elem2).find('a').text(),
+                            permalink: $(elem2).find('a').attr('href')
+                        }
+                    });
+
+                    lists[i] = {
+                        rank: $(elem).find('.rank span').text(),
+                        name: $(elem).find('.company .information a').text(),
+                        alias: $(elem).find('.company .information small').text(),
+                        amount: $(elem).find('.numofanime').text().replace(/\s\s+/g, ' ').trim(),
+                        mostPopulars: mostPopulars,
+                        topScorers: topScorers,
+                        favorites: $(elem).find('.favorites').text().replace(/\s\s+/g, ' ').trim(),
+                        permalink: $(elem).find('.company .information a').attr('href'),
+                        image: $(elem).find('.company img').attr('data-src')
+                    }
+                });
+                lists = lists.filter(n => n !== undefined);
+                res.send(lists);
+            }
+        })
+        .catch(error => {
+            console.log(error);
+        })
 }
--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 import express from 'express';
-import {Endpoints, Top} from '../controllers/index.js';
+import {Endpoints, Top, People, Characters, Companies} from '../controllers/index.js';
 
 const router = express.Router();
 
 router.get('/', Endpoints);
 router.get('/top/:category/:type', Top);
+router.get('/people', People);
+router.get('/characters', Characters);
+router.get('/companies', Companies);
 
 router.use((req, res, next) => {
     res.status(404).send("Route is not found!");
