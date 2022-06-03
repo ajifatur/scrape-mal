e1735541111499fff3463244979487a5214bd1b2
Aji Fatur <ajifatur32@gmail.com>
2022-06-03 10:45:51 +0700

    Add cors

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
 import express from 'express';
+import cors from 'cors';
 import router from './routes/index.js';
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use(cors({
+    origin: '*'
+}));
 
 app.use(router);
 
